54ee36517b830066480f5b79d40eb2fb152eff11
Nikita <nik.mashcov13@mail.ru>
2022-07-11 14:02:03 +0400

    add label

--- my-app/src/components/NotFoundPage.jsx
+++ my-app/src/components/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const NotFoundPage = () => {
-  return <div className='text-center'>NotFoundPage</div>;
+  return <div className='text-center'>NotFoundPage 404</div>;
 };

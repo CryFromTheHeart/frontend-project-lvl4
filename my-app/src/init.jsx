6aba8237ebbb28e4779957903d0f48827eb5c217
CryFromTheHeart <nik.mashcov13@mail.ru>
2022-07-11 18:03:53 +0400

    and another try

--- my-app/src/init.jsx
+++ my-app/src/init.jsx
@@ -31,25 +31,49 @@ const init = async () => {
     lng: 'ru',
     resources,
   });
-  const actionsWithSocket = {
-    sendMessage: (message) => {
-      let state = 'pending'; // eslint-disable-line
-      const timer = setTimeout(() => {
-        state = 'rejected';
-        return;
-      }, 3000);
-      const form = document.querySelector('#messages-box');
-      socket.volatile.emit('newMessage', message, (response) => {
-        if (state !== 'pending') return;
-        form.innerHTML = `${response.status}`;
-        clearTimeout(timer);
 
-        if (response.status === 'ok') {
-          state = 'resolved';
-          return response.data;
-        }
+  const withAcknowledgement =
+    (socketFunc) =>
+    (...args) =>
+      new Promise((resolve, reject) => {
+        let state = 'pending'; // eslint-disable-line
+        const timer = setTimeout(() => {
+          state = 'rejected';
+          reject();
+        }, 3000);
+
+        socketFunc(...args, (response) => {
+          if (state !== 'pending') return;
+
+          clearTimeout(timer);
+          const form = document.querySelector('#messages-box');
+          form.innerHTML = response.status;
+          if (response.status === 'ok') {
+            state = 'resolved';
+            resolve(response.data);
+          }
+
+          reject();
+        });
       });
-    },
+
+  const api = {
+    sendMessage: withAcknowledgement((...args) =>
+      socket.volatile.emit('newMessage', ...args)
+    ),
+    addChannel: withAcknowledgement((...args) =>
+      socket.volatile.emit('newChannel', ...args)
+    ),
+    renameChannel: withAcknowledgement((...args) =>
+      socket.volatile.emit('renameChannel', ...args)
+    ),
+    removeChannel: withAcknowledgement((...args) =>
+      socket.volatile.emit('removeChannel', ...args)
+    ),
+  };
+
+  const actionsWithSocket = {
+    sendMessage: (message) => socket.volatile.emit('newMessage', message),
     addChannel: (channel) => socket.emit('newChannel', channel),
     removeChannel: (channel) => socket.emit('removeChannel', channel),
     renameChannel: (channel) => socket.emit('renameChannel', channel),

96e3efb743e6849ddec9cb011a2dcac49d67e55d
CryFromTheHeart <nik.mashcov13@mail.ru>
2022-07-12 14:41:24 +0400

    rremove comments

--- my-app/src/init.jsx
+++ my-app/src/init.jsx
@@ -33,45 +33,6 @@ const init = async () => {
     resources,
   });
 
-  /* const withAcknowledgement =
-    (socketFunc) =>
-    (...args) =>
-      new Promise((resolve, reject) => {
-        let state = 'pending'; // eslint-disable-line
-        const timer = setTimeout(() => {
-          state = 'rejected';
-          reject();
-        }, 3000);
-
-        socketFunc(...args, (response) => {
-          if (state !== 'pending') return;
-
-          clearTimeout(timer);
-
-          if (response.status === 'ok') {
-            state = 'resolved';
-            resolve(response.data);
-          }
-
-          reject();
-        });
-      });
-
-  const api = {
-    sendMessage: withAcknowledgement((...args) =>
-      socket.volatile.emit('newMessage', ...args)
-    ),
-    addChannel: withAcknowledgement((...args) =>
-      socket.volatile.emit('newChannel', ...args)
-    ),
-    renameChannel: withAcknowledgement((...args) =>
-      socket.volatile.emit('renameChannel', ...args)
-    ),
-    removeChannel: withAcknowledgement((...args) =>
-      socket.volatile.emit('removeChannel', ...args)
-    ),
-  }; */
-
   const actionsWithSocket = {
     sendMessage: (message) => socket.volatile.emit('newMessage', message),
     addChannel: (channel) => socket.emit('newChannel', channel),
